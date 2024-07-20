5708c44bb3c2e3a8b4e6bf49e39aec7fe786d672
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-07-20 16:59:59 +0300

    add router

--- src/server.js
+++ src/server.js
@@ -2,49 +2,17 @@ import dotenv from 'dotenv';
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { Contact } from './models/contacts.js';
 
 dotenv.config();
 
 export const setupServer = () => {
+  
   const app = express();
 
   app.use(cors());
   app.use(pino());
 
-  app.get('/contacts', async (req, res) => {
-      try {
-        const contacts = await Contact.find();
-        res.status(200).json({
-          status: 200,
-          message: 'Successfully found contacts!',
-          data: contacts,
-        });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      }
-    });
-
-    app.get('/contacts/:contactId', async (req, res) => {
-      try {
-        const { contactId } = req.params;
-        const user = await Contact.findById(contactId);
-
-        if (user === null) {
-          return res.status(404).json({ status: 404, message: 'Contact not found' });
-        }
-
-        res.status(200).json({
-          status: 200,
-          message: `Successfully found contact with id ${contactId}!`,
-          data: user,
-        });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      }
-    });
+  const PORT = process.env.PORT || 3000;
 
   app.use('*', (req, res) => {
     res.status(404).json({
@@ -58,8 +26,6 @@ export const setupServer = () => {
       error: err.message,
     });
   });
-
-  const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
