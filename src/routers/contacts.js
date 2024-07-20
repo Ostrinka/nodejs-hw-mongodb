22fee32e80c3d2d9a09a246f23409bdf618d9e19
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-07-20 19:22:14 +0300

    add ctrlWrapper

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getStudentsController, getStudentByIdController } from "../controllers/contacts.js";
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.get('/contacts', getStudentsController);
+router.get('/contacts', ctrlWrapper(getStudentsController));
 
-router.get('/contacts/:contactId', getStudentByIdController);
+router.get('/contacts/:contactId', ctrlWrapper(getStudentByIdController));
 
 export default router;
